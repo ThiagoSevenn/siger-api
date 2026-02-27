8220070d3bf0d8b9b61f0dc97eb0de562b77a035
ThiagoSevenn <thiagonevessc@gmail.com>
2026-02-27 09:33:00 -0300

    Criação do projeto(Apenas dependências)

---
+++ src/main/java/br/com/siger/siger_api/SigerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.siger.siger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/siger/siger_api/SigerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.siger.siger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
