1af8c119e49bbfba28a18a7f393302289f777388
Bebeto03 <rnxroberto@gmail.com>
2026-04-09 12:00:27 -0300

    feat: implemente autenticacaoJWT e roles

---
+++ src/main/java/br/com/siger/siger_api/service/AuthService.java
@@ -0,0 +1,24 @@
+package br.com.siger.siger_api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.siger.siger_api.repository.UserRepository;
+
+@Service
+public class AuthService implements UserDetailsService{
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username);
+    }
+    
+
+
+}
