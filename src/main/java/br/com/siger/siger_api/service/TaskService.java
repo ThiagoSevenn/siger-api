5df04f8569a65d2209c42f678424bb6374648dfb
Bebeto03 <rnxroberto@gmail.com>
2026-04-27 15:07:21 -0300

    inclusão da anotação service faltante

--- src/main/java/br/com/siger/siger_api/service/TaskService.java
+++ src/main/java/br/com/siger/siger_api/service/TaskService.java
@@ -1,8 +1,11 @@
 package br.com.siger.siger_api.service;
 
+import org.springframework.stereotype.Service;
+
 import br.com.siger.siger_api.global.service.GenericBaseService;
 import br.com.siger.siger_api.model.Task;
 import br.com.siger.siger_api.repository.TaskRepository;
 
+@Service
 public class TaskService extends GenericBaseService<TaskRepository, Task, Long> {
 }
